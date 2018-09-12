8823b5e871a31fc4d164286ebc65e7bc85d2e14d
desmondmm <mutsakaradesmond@gmail.com>
2018-09-12 05:02:15 +0200

    Created register and blood user classes

---
+++ BloodTrace/BloodTrace/Models/BloodUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BloodTrace.Models
+{
+    class BloodUser
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Country { get; set; }
+        public string BloodGroup { get; set; }
+        public string ImagePath { get; set; }
+        public int Date { get; set; }
+        public object ImageArray { get; set; }
+    }
+}
