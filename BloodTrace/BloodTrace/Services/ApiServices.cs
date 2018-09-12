a97747a861fe9dab312bd9eeb973d0088ac4424c
desmondmm <mutsakaradesmond@gmail.com>
2018-09-12 05:17:33 +0200

    Added a service to create users

---
+++ BloodTrace/BloodTrace/Services/ApiServices.cs
@@ -0,0 +1,29 @@
+﻿using BloodTrace.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodTrace.Services
+{
+    class ApiServices
+    {
+        public async Task<bool> RegisterUser(string email, string password, string confirmPassword)
+        {
+            var registerModel = new RegisterModel()
+            {
+                Email = email,
+                Password = password,
+                ConfirmPasword = confirmPassword
+            };
+
+            var httpClient = new HttpClient();
+            var json = JsonConvert.SerializeObject(registerModel);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await httpClient.PostAsync("http://localhost:50268/api/Account/Register", content);
+            return response.IsSuccessStatusCode;
+        }
+    }
+}
