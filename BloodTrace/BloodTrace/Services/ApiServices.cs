5bd8b3584ea6e40ef5325da105187c894408be80
desmondmm <mutsakaradesmond@gmail.com>
2018-09-12 05:27:42 +0200

    Added code to login users

--- BloodTrace/BloodTrace/Services/ApiServices.cs
+++ BloodTrace/BloodTrace/Services/ApiServices.cs
@@ -25,5 +25,22 @@ namespace BloodTrace.Services
             var response = await httpClient.PostAsync("http://localhost:50268/api/Account/Register", content);
             return response.IsSuccessStatusCode;
         }
+
+        public async Task<bool> LoginUser(string email, string password)
+        {
+            var keyvalues = new List<KeyValuePair<string, string>>()
+            {
+                new KeyValuePair<string, string>("username", email),
+                new KeyValuePair<string, string>("password", password),
+                new KeyValuePair<string, string>("grant_type", "password"),
+            };
+
+            var request = new HttpRequestMessage(HttpMethod.Post, "http://localhost:50268/Token");
+            request.Content = new FormUrlEncodedContent(keyvalues);
+            var httpClient = new HttpClient();
+            var response = await httpClient.SendAsync(request);
+            var content = response.Content.ReadAsStringAsync();
+            return response.IsSuccessStatusCode;
+        }
     }
 }
