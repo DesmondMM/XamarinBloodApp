d3f0cca20837a213164a12d83a4ef71dcb48154a
desmondmm <mutsakaradesmond@gmail.com>
2018-09-13 04:31:24 +0200

    Added blood reg form and added camera funtionality

--- BloodTrace/BloodTrace/Pages/RegisterBlood.xaml.cs
+++ BloodTrace/BloodTrace/Pages/RegisterBlood.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +14,43 @@ namespace BloodTrace.Pages
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class RegisterBlood : ContentPage
 	{
-		public RegisterBlood ()
+        public MediaFile file;
+        public RegisterBlood ()
 		{
 			InitializeComponent ();
 		}
-	}
+
+        private void BtnSubmit_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void TapOpenCamera_Tapped(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await DisplayAlert("No Camera", ":( No camera available.", "OK");
+                return;
+            }
+
+            file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+            {
+                Directory = "Sample",
+                Name = "test.jpg"
+            });
+
+            if (file == null)
+                return;
+
+            await DisplayAlert("File Location", file.Path, "OK");
+
+            ImgDonor.Source = ImageSource.FromStream(() =>
+            {
+                var stream = file.GetStream();
+                return stream;
+            });
+        }
+    }
 }
