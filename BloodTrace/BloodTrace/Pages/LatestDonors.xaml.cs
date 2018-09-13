46a7a0333e6d4b285bade58b6f8c76330bf771ac
desmondmm <mutsakaradesmond@gmail.com>
2018-09-13 06:02:59 +0200

    Latest donor page done

--- BloodTrace/BloodTrace/Pages/LatestDonors.xaml.cs
+++ BloodTrace/BloodTrace/Pages/LatestDonors.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using BloodTrace.Models;
+using BloodTrace.Services;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +15,30 @@ namespace BloodTrace.Pages
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class LatestDonors : ContentPage
 	{
-		public LatestDonors ()
+        private ObservableCollection<BloodUser> BloodUsers;
+        public LatestDonors ()
 		{
-			InitializeComponent ();
-		}
-	}
+            InitializeComponent();
+            BloodUsers = new ObservableCollection<BloodUser>();
+            FindBloodDonors();
+        }
+
+        private async void FindBloodDonors()
+        {
+            ApiServices apiServices = new ApiServices();
+            var bloodUsers = await apiServices.LatestBloodUser();
+            foreach (var bloodUser in bloodUsers)
+            {
+                BloodUsers.Add(bloodUser);
+            }
+
+            LvBloodDonors.ItemsSource = BloodUsers;
+        }
+
+        private void LvBloodDonors_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var selectedDonor = e.SelectedItem as BloodUser;
+            Navigation.PushAsync(new DonorProfilePage(selectedDonor));
+        }
+    }
 }
